9bc2875f8159fc9279880b39e011f776d72498d7
Zdh-501 <1434827791@qq.com>
2024-01-27 13:33:58 +0800

    Merge remote-tracking branch 'origin/master'

--- ui/impl/myThread.py
+++ ui/impl/myThread.py
@@ -149,14 +149,14 @@ class LabelThread(QThread):
         return self.process and self.process.poll() is None
 
 class ImageSaveThread(QThread):
-    save_finished = pyqtSignal(str)  # 用于在保存完成后发送信号
+    save_finished = pyqtSignal(str)  # 用于保存完成后发出信号
 
     def __init__(self, image, file_path, file_format):
         super().__init__()
         self.image = image
         self.file_path = file_path
         self.file_format = file_format
-        self.file_path = self.file_path.encode('utf-8').decode(sys.getfilesystemencoding())
+
     def run(self):
         # 使用pathlib处理路径
         save_path = Path(self.file_path)
@@ -165,7 +165,7 @@ class ImageSaveThread(QThread):
         save_path.parent.mkdir(parents=True, exist_ok=True)
 
         # 定义缩放比例（例如，缩小到原来的50%）
-        scale_percent = 50  # 缩放比例
+        scale_percent = 50
         width = int(self.image.shape[1] * scale_percent / 100)
         height = int(self.image.shape[0] * scale_percent / 100)
         dim = (width, height)
@@ -173,12 +173,16 @@ class ImageSaveThread(QThread):
         # 缩放图像
         resized_image = cv2.resize(self.image, dim, interpolation=cv2.INTER_AREA)
 
-        # 使用cv2保存图像
-        if self.file_format.lower() == 'png':
-            cv2.imwrite(str(save_path), resized_image, [int(cv2.IMWRITE_PNG_COMPRESSION), 6])
-        elif self.file_format.lower() == 'jpg' or self.file_format.lower() == 'jpeg':
-            cv2.imwrite(str(save_path), resized_image, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+        # 将图像编码为指定的格式
+        ext = f'.{self.file_format.lower()}'
+        result, encoded_image = cv2.imencode(ext, resized_image)
+        if result:
+            # 将编码后的图像写入文件
+            try:
+                with open(save_path, 'wb') as file:
+                    file.write(encoded_image)
+                self.save_finished.emit(f"图像已保存到 {save_path}")
+            except Exception as e:
+                self.save_finished.emit(f"保存图像失败: {e}")
         else:
-            self.save_finished.emit("不支持的文件格式")
-            return
-        self.save_finished.emit(f"图像已保存到 {str(save_path)}")
+            self.save_finished.emit("图像编码失败")
