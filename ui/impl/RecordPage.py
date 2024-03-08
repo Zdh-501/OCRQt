5a0b3b5ee8d94e267e29b810c056e022e51f43f0
Zdh-501 <1434827791@qq.com>
2024-03-08 22:16:49 +0800

    修正bug,包括上传信息task_key值重复； 修改回查记录功能

--- ui/impl/RecordPage.py
+++ ui/impl/RecordPage.py
@@ -4,7 +4,7 @@ import pyodbc
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import Qt,QSize
 from PyQt5.QtGui import QPixmap, QIcon, QImageReader, QFont, QImage
-from PyQt5.QtWidgets import QListWidgetItem, QMessageBox, QGraphicsPixmapItem, QGraphicsScene, QApplication
+from PyQt5.QtWidgets import QTableWidgetItem, QCheckBox, QListWidgetItem, QMessageBox, QGraphicsPixmapItem, QGraphicsScene, QApplication
 from pyqt5_plugins.examplebuttonplugin import QtGui
 
 from ui.layout.UI_RecordPage import Ui_RecordPage
@@ -15,6 +15,7 @@ class RecordPage(QtWidgets.QWidget, Ui_RecordPage):
     def __init__(self):
         super(RecordPage, self).__init__()
         self.setupUi(self)  # 从UI_RecordPage.py中加载UI定义
+
         # 初始化 QGraphicsScene
         self.scene = QGraphicsScene(self)
         self.graphicsView.setScene(self.scene)
@@ -22,20 +23,23 @@ class RecordPage(QtWidgets.QWidget, Ui_RecordPage):
 
         self.results_data = []  # 用于存储查询结果的全局变量
 
-        # 设置 listWidget 的图标尺寸
-        self.listWidget.setIconSize(QSize(100, 100))  # 将图标尺寸设置为 100x100
-
+        self.tableWidget.setSortingEnabled(True)
+        self.tableWidget.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
+        self.tableWidget.setColumnCount(4)  # 依据您的列数
+        self.tableWidget.setHorizontalHeaderLabels(
+            ['勾选框','任务标识符', '工单号', '批号'])
         # 使用关键字参数连接到数据库
         self.db = dbConnect()
         # 连接按钮信号
         self.pushButton_1.clicked.connect(self.query_records)
-        self.pushButton_2.clicked.connect(lambda: self.sort_records(True))
-        self.pushButton_5.clicked.connect(lambda: self.sort_records(False))
+
         self.pushButton_6.clicked.connect(self.delete_checked_items)
         # 连接全选复选框的状态改变信号
         self.checkBox.stateChanged.connect(self.toggleSelectAll)
         # 连接列表项点击信号
-        self.listWidget.itemClicked.connect(self.displayImage)
+        self.tableWidget.cellClicked.connect(self.displayImage)
+
         self.pushButton_4.clicked.connect(lambda: self.scaleImage(1.25))  # 放大
         self.pushButton_3.clicked.connect(lambda: self.scaleImage(0.8))  # 缩小
 
@@ -43,11 +47,10 @@ class RecordPage(QtWidgets.QWidget, Ui_RecordPage):
         self.dateTimeEdit_1.setDateTime(QtCore.QDateTime.currentDateTime().addDays(-30))  # 默认起始时间为30天前
         self.dateTimeEdit_2.setDateTime(QtCore.QDateTime.currentDateTime())  # 默认结束时间为当前时间
 
-    def displayImage(self, item):
-        # 获取选中项的索引
-        index = self.listWidget.row(item)
+    def displayImage(self, row, column):
+
         # 获取保存的结果数据
-        data = self.results_data[index]
+        data = self.results_data[row]
         image_data = data[6]  # 假定IMAGE字段在结果中的索引为6
 
         try:
@@ -63,19 +66,18 @@ class RecordPage(QtWidgets.QWidget, Ui_RecordPage):
                 self.graphicsView.fitInView(self.pixmapItem, Qt.KeepAspectRatio)
         except Exception as e:
             print(f"在 displayImage 方法中发生错误: {e}")
-        self.displayProductInfo()
+        self.displayProductInfo(row)
 
-    def displayProductInfo(self):
-        # 获取最后一次点击的项目的索引
-        index = self.listWidget.currentRow()
+    def displayProductInfo(self,index):
+
         # 获取保存的结果数据
         data = self.results_data[index]
         task_identifier, sequence, order_no, batch_no, production_date, expiry_date, image_data, cwid, operationtime = data
 
         try:
             info = (f"任务标识符: {task_identifier}\n"
-                    f"序列: {sequence}\n"
-                    f"订单号: {order_no}\n"
+                    f"序号: {sequence}\n"
+                    f"工单号: {order_no}\n"
                     f"批号: {batch_no}\n"
                     f"生产日期: {production_date}\n"
                     f"有效期至: {expiry_date}\n"
@@ -94,50 +96,46 @@ class RecordPage(QtWidgets.QWidget, Ui_RecordPage):
             currentScale = self.pixmapItem.scale()
             self.pixmapItem.setScale(currentScale * factor)
 
-
-
-
     def query_records(self):
         start_time = self.dateTimeEdit_1.dateTime().toString("yyyy-MM-dd HH:mm:ss")
         end_time = self.dateTimeEdit_2.dateTime().toString("yyyy-MM-dd HH:mm:ss")
-        batch_no_filter = self.lineEdit.text().strip()  # 更改为使用批号过滤
+        cwid_filter = self.lineEdit.text().strip()  # 使用 CWID 过滤
 
         try:
             with self.db.cursor() as cursor:
-                # 构建查询语句以选择所有字段
                 query = """SELECT TASK_IDENTIFIER, SEQUENCE, ORDER_NO, BATCH_NO, PRODUCTION_DATE, 
                                 EXPIRY_DATE, IMAGE, CWID, OPERATIONTIME 
                            FROM dbo.ResultTable 
                            WHERE OPERATIONTIME BETWEEN ? AND ?"""
                 params = [start_time, end_time]
 
-                # 如果有输入批号，则添加筛选条件
-                if batch_no_filter:
-                    query += " AND BATCH_NO = ?"
-                    params.append(batch_no_filter)
+                if cwid_filter:  # 如果 CWID 过滤文本框内有内容
+                    query += " AND CWID = ?"  # 添加 CWID 到 WHERE 条件
+                    params.append(cwid_filter)
 
-                # 执行查询
                 cursor.execute(query, params)
                 self.results_data = cursor.fetchall()
-                self.listWidget.clear()  # 清空当前列表
+                self.tableWidget.setRowCount(0)  # 清空表格
 
-                for row in self.results_data:
-                    task_identifier, sequence, order_no, batch_no, production_date, \
-                        expiry_date, image_data, cwid, operationtime = row
+                for row_index, row_data in enumerate(self.results_data):
+                    self.tableWidget.insertRow(row_index)
 
-                    # 解析 IMAGE 字段数据
-                    pixmap = self.convert_image_data(image_data)
-                    if pixmap is None:  # 如果解析失败，则跳过当前项
-                        continue
+                    # 创建一个包含复选框的QWidget
+                    checkbox_widget = QtWidgets.QWidget()
+                    checkbox_layout = QtWidgets.QHBoxLayout(checkbox_widget)
+                    checkbox = QtWidgets.QCheckBox()
+                    checkbox_layout.addWidget(checkbox)
+                    checkbox_layout.setAlignment(QtCore.Qt.AlignCenter)
+                    checkbox_layout.setContentsMargins(0, 0, 0, 0)
+                    self.tableWidget.setCellWidget(row_index, 0, checkbox_widget)
 
-                    icon = QIcon(pixmap.scaled(128, 128, Qt.KeepAspectRatio, Qt.SmoothTransformation))
-                    item_text = f"{task_identifier} ({order_no}) - {batch_no}"
-                    item = QListWidgetItem(icon, item_text)
-                    self.listWidget.addItem(item)
+                    # 为TASK_IDENTIFIER, ORDER_NO, BATCH_NO设置QTableWidgetItem
+                    for col_index, data in enumerate([row_data[0], row_data[2], row_data[3]], start=1):
+                        item = QtWidgets.QTableWidgetItem(str(data))
+                        item.setTextAlignment(QtCore.Qt.AlignCenter)  # 设置文本居中
+                        self.tableWidget.setItem(row_index, col_index, item)
 
-                    item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
-                    item.setCheckState(Qt.Unchecked)
-                    self.listWidget.addItem(item)
+                    self.tableWidget.setRowHeight(row_index, 40)  # 设置行高
         except Exception as e:
             QMessageBox.critical(self, "数据库错误", str(e))
 
@@ -152,43 +150,54 @@ class RecordPage(QtWidgets.QWidget, Ui_RecordPage):
                 return None
             return QPixmap.fromImage(image)
         return None
+
     def toggleSelectAll(self, state):
-        for i in range(self.listWidget.count()):
-            item = self.listWidget.item(i)
-            item.setCheckState(Qt.Checked if state == Qt.Checked else Qt.Unchecked)
-    def sort_records(self, ascending):
-        self.listWidget.sortItems(QtCore.Qt.AscendingOrder if ascending else QtCore.Qt.DescendingOrder)
+        for row in range(self.tableWidget.rowCount()):
+            # 获取包含复选框的QWidget
+            checkbox_widget = self.tableWidget.cellWidget(row, 0)
+            # 确保获取到了QWidget
+            if checkbox_widget:
+                # 从布局中获取QCheckBox
+                checkbox = checkbox_widget.layout().itemAt(0).widget()
+                # 确保获取到的是QCheckBox，然后设置其状态
+                if isinstance(checkbox, QtWidgets.QCheckBox):
+                    checkbox.setChecked(state == Qt.Checked)
 
     def delete_checked_items(self):
-        # 检查是否有勾选项
-        has_checked_items = any(self.listWidget.item(i).checkState() == Qt.Checked
-                                for i in range(self.listWidget.count()))
+        try:
+            checked_rows = []
+            with self.db.cursor() as cursor:
+                for row in range(self.tableWidget.rowCount()):
+                    # 获取包含复选框的QWidget
+                    checkbox_widget = self.tableWidget.cellWidget(row, 0)
+                    # 从布局中获取复选框
+                    checkbox = checkbox_widget.layout().itemAt(0).widget()
+                    if checkbox.isChecked():
+                        checked_rows.append(row)
 
-        if not has_checked_items:
-            QMessageBox.information(self, "提示", "请勾选要删除的项")
-            return
-        else:
-            # 弹出确认对话框
-            reply = QMessageBox.question(self, '确认删除', '您确定要删除勾选的项吗？',
-                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+                if not checked_rows:
+                    QMessageBox.information(self, "提示", "请勾选要删除的项")
+                    return
 
-            if reply == QMessageBox.Yes:
-                try:
-                    with self.db.cursor() as cursor:
-                        for i in range(self.listWidget.count() - 1, -1, -1):  # 倒序遍历，避免索引问题
-                            item = self.listWidget.item(i)
-                            if item.checkState() == Qt.Checked:
-                                image_path = item.data(Qt.UserRole)
-                                query = "DELETE FROM products WHERE image_path = %s"
-                                cursor.execute(query, (image_path,))  # 使用参数化查询
-                                self.db.commit()
+                reply = QMessageBox.question(self, '确认删除', '您确定要删除勾选的项吗？',
+                                             QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 
-                                # 从列表中移除项
-                                self.listWidget.takeItem(i)
+                if reply == QMessageBox.Yes:
+                    for row in sorted(checked_rows, reverse=True):
+                        # 假设OPERATIONTIME字段是用于删除的标识符，并且它在self.results_data中的索引是8
+                        operation_time = self.results_data[row][-1]  # 使用-1假设OPERATIONTIME在最后
+                        query = "DELETE FROM dbo.ResultTable WHERE OPERATIONTIME = ?"
+                        cursor.execute(query, (operation_time,))
+                        self.db.commit()
 
-                except Exception as e:
-                    QMessageBox.critical(self, "数据库错误", str(e))
-                    self.db.rollback()
+                        # 从表中移除行
+                        self.tableWidget.removeRow(row)
+                        # 同步移除self.results_data中的数据
+                        self.results_data.pop(row)
+
+        except Exception as e:
+            QMessageBox.critical(self, "数据库错误", str(e))
+            self.db.rollback()
 
     def perform_deletion(self, selected_items):
         try:
